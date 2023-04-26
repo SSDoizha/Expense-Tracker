e997ce16c00153a797f0b774b8af2235f9b9c294
SSDoizha <serhiidoizha@gmail.com>
2023-04-26 21:20:54 +0300

    adding first css code
    we create and import some css to our ExpenseItem.js

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,4 +1,14 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h1>ExpenseItem is here!</h1>;
+  return (
+    <div className="expense-item">
+      <div>28 march 2020</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$348</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
