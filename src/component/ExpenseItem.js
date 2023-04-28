392305d141af3637f6dd82530877565713049886
SSDoizha <serhiidoizha@gmail.com>
2023-04-28 22:09:51 +0300

    Adding "normal" JavaScript Logic to Components
    adding some logic to date width javaScript

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const day = props.date.toLocaleString("en-Us", { day: "numeric" });
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
